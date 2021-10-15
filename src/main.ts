b2c1d8dc574f71c799f26585d527c9a3c0688673
ABIERRE <ishibashi.kazuya@abierre.com>
2021-10-15 15:38:30 +0900

    20211015_15-38-30

--- src/main.ts
+++ src/main.ts
@@ -5,4 +5,29 @@ import router from './router'
 import store from './store'
 import 'destyle.css'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+
+app.config.performance = true
+app.config.warnHandler = () => undefined
+app.config.errorHandler = () => undefined
+
+const isDevelopment = false
+if (!isDevelopment) {
+  console.log = () => { return false }
+  console.warn = () => { return false }
+  console.error = () => { return false }
+  console.debug = () => { return false }
+  console.info = () => { return false }
+  console.dir = () => { return false }
+  console.dirxml = () => { return false }
+  console.group = () => { return false }
+  console.groupCollapsed = () => { return false }
+  console.groupEnd = () => { return false }
+  console.table = () => { return false }
+  console.time = () => { return false }
+  console.timeEnd = () => { return false }
+  console.timeStamp = () => { return false }
+  console.trace = () => { return false }
+}
+
+app.use(store).use(router).mount('#app')
